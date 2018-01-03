81e8404784c7a6ffb65499cc564a76f512cf50cd
jared <jcrean01@gmail.com>
2018-01-03 12:04:14 -0500

    remove unneeded output from install script

--- deps/install_petsc.sh
+++ deps/install_petsc.sh
@@ -85,10 +85,3 @@ echo "export PETSC_ARCH=$PETSC_ARCH" >> use_petsc.sh
 
 echo "$petsc_dir" > petsc_evars
 echo "$PETSC_ARCH" >> petsc_evars
-
-echo "pwd = "
-echo `pwd`
-echo "ls = "
-echo `ls`
-
-
