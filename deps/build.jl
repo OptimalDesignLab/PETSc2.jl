a6f096786d20658a2146f8a9339cf37016c4a98c
Jared Crean <jcrean01@gmail.com>
2018-05-24 16:24:39 -0400

    start upgrade to 0.5

--- deps/build.jl
+++ deps/build.jl
@@ -10,7 +10,7 @@ if !haskey(pkg_dict, "ArrayViews")
   Pkg.clone("https://github.com/JuliaLang/ArrayViews.jl.git")
   start_dir = pwd()
   cd(Pkg.dir("ArrayViews"))
-  run(`git checkout 93e80390aeedb1dbcd90281b6dff7f760f430bc8`)
+  run(`git checkout 715d637fbdd0f6dca6a03d1f2d7c8d248b4abde8`)
   cd (start_dir)
 end
 
@@ -18,7 +18,7 @@ if !(haskey(pkg_dict, "FactCheck"))
   Pkg.clone("FactCheck")
   start_dir = pwd()
   cd(Pkg.dir("FactCheck"))
-  run(`git checkout e3739d5fdf0e54bc1e74957c060c693cd8ce9cd6`)
+  run(`git checkout 66fc925bd99b50aacfa0f9cd2c324f80ec4947f3`)
   cd(start_dir)
   Pkg.build("FactCheck")
 end
