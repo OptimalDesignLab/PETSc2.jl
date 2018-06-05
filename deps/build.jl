8396a6ea2bec072737ba554db633f2e8f86a8623
Jared Crean <jcrean01@gmail.com>
2018-06-05 13:13:33 -0400

    change PkgFix installation

--- deps/build.jl
+++ deps/build.jl
@@ -2,7 +2,10 @@
 if !isdir(joinpath(Pkg.dir(), "PkgFix"))
   Pkg.clone("https://github.com/OptimalDesignLab/PkgFix.jl.git")
 end
-Pkg.checkout("PkgFix", "upgrade_0.6")
+start_dir=pwd()
+cd(Pkg.dir("PkgFix"))
+run(`git checkout upgrade_0.6`)
+cd(start_dir)
 
 using PkgFix
 
