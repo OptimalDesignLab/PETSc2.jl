b35649e9ec0cad6454ad24c11f618043233d7693
Jared Crean <jcrean01@gmail.com>
2019-08-12 13:13:18 -0400

    try petsc 3.9.4

--- deps/download.sh
+++ deps/download.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 # download petsc ahead of time if needed
 
-petsc_name="petsc-3.8.4"
+petsc_name="petsc-3.9.4"
 fmt=".tar.gz"
 url="http://ftp.mcs.anl.gov/pub/petsc/release-snapshots/"
 
--- deps/install_petsc.sh
+++ deps/install_petsc.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 set -ex
-petsc_name=petsc-3.8.4
+petsc_name=petsc-3.9.4
 fmt=.tar.gz
 
 echo "building PETSc in `pwd`"
